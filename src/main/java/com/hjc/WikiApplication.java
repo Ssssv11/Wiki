f5f4ce9f5ada995bc45ec85863d8b41e15f27e3e
Ssssv11 <1416431087@qq.com>
2022-08-12 12:18:41 +0800

    启动日志优化

--- src/main/java/com/hjc/WikiApplication.java
+++ src/main/java/com/hjc/WikiApplication.java
@@ -1,13 +1,20 @@
 package com.hjc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
 
 @SpringBootApplication
 public class WikiApplication {
+    private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(WikiApplication.class, args);
+        Environment env = SpringApplication.run(WikiApplication.class, args).getEnvironment();
+        LOG.info("Start Success!");
+        LOG.info("At: \thttp://127.0.0.1:{}", env.getProperty("server port"));
     }
 
 }
