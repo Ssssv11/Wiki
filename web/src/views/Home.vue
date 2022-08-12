9b87c4e3e9326cfa844e483d5ade227557839368
Ssssv11 <1416431087@qq.com>
2022-08-12 17:05:56 +0800

    修改axios baseURL

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -94,7 +94,7 @@ export default defineComponent({
     const ebooks = ref();
     const ebooks1 = reactive({books:[]});
     onMounted(() => {
-      axios.get(process.env.VUE_APP_SERVER + "/ebook/list").then((response) => {
+      axios.get("/ebook/list").then((response) => {
         console.log(response);
         const data = response.data;
         ebooks.value = data.content;
