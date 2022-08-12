e5d87f71131f929e30cf508be9c93a89c7470d45
Ssssv11 <1416431087@qq.com>
2022-08-12 14:41:33 +0800

    用CopyUtil封装BeanUtils

--- src/main/java/com/hjc/service/EbookService.java
+++ src/main/java/com/hjc/service/EbookService.java
@@ -5,11 +5,10 @@ import com.hjc.domain.EbookExample;
 import com.hjc.mapper.EbookMapper;
 import com.hjc.req.EbookReq;
 import com.hjc.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.hjc.utils.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,12 +21,6 @@ public class EbookService {
         EbookExample.Criteria criteria = ebookExample.createCriteria();
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        List<EbookResp> respList = new ArrayList<>();
-        for(Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-        return respList;
+        return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
