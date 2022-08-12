9b5384e116ac226170b3339a1aba395eb232010c
Ssssv11 <1416431087@qq.com>
2022-08-12 15:39:12 +0800

    制作header与footer组件

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-layout-footer style="text-align: center">
+    Ssssv 📖 Wiki
+  </a-layout-footer>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
