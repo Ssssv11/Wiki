e569bf1a3f012fe2ec7d8066bb026d4eb8482546
Ssssv11 <1416431087@qq.com>
2022-08-12 23:54:35 +0800

    分类表设计与持久层代码生成

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -131,7 +131,7 @@ export default defineComponent({
         dataIndex: 'voteCount'
       },
       {
-        title: 'Action',
+        title: '操作',
         key: 'action',
         slots: { customRender: 'action' }
       }
