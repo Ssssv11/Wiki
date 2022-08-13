c4158ef1b79e9cd66c92cfcb832961d6605aa5fa
Ssssv11 <1416431087@qq.com>
2022-08-13 16:46:13 +0800

    增加Doc页面，点击首页Ebook可进入Doc

--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -4,6 +4,7 @@ import About from '../views/About.vue'
 import AdminEbook from '../views/admin/AdminEbook.vue'
 import AdminCategory from "@/views/admin/AdminCategory.vue";
 import AdminDoc from "@/views/admin/AdminDoc.vue";
+import Doc from "@/views/Doc.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -30,6 +31,11 @@ const routes: Array<RouteRecordRaw> = [
     path: '/admin/doc',
     name: 'AdminDoc',
     component: AdminDoc
+  },
+  {
+    path: '/doc',
+    name: 'Doc',
+    component: Doc
   },
 ]
 
