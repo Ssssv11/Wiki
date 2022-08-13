59a15bc43f3ed286ee17dcc14f4a07b2f65a1fcd
Ssssv11 <1416431087@qq.com>
2022-08-13 14:06:57 +0800

    增加页面参数传递完成新增文档功能

--- web/src/views/admin/AdminDoc.vue
+++ web/src/views/admin/AdminDoc.vue
@@ -87,10 +87,12 @@ import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import {Tool} from '@/utils/tool.ts'
+import {useRoute} from "vue-router";
 
 export default defineComponent({
   name: 'AdminDoc',
   setup() {
+    const route = useRoute();
     const param = ref();
     param.value = {};
     const docs = ref();
@@ -173,7 +175,9 @@ export default defineComponent({
 
     const add = () => {
       modalVisible.value = true;
-      doc.value = {};
+      doc.value = {
+        ebookId: route.query.ebookId
+      };
       treeSelectData.value = Tool.copy(level1.value);
       treeSelectData.value.unshift({id: 0, name: '无'});
     };
