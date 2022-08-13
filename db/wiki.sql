45477ea4cd310b3eb885035445d8267e9776e47f
Ssssv11 <1416431087@qq.com>
2022-08-14 02:21:25 +0800

    添加ebook快照表生成持久层代码

--- db/wiki.sql
+++ db/wiki.sql
@@ -112,3 +112,16 @@ create table `user` (
 )  engine = innodb default charset = utf8mb4 comment '用户';
 
 insert into `user` (`id`, `login_name`, `name`, `password`) values (1, 'test', 'test', 'test');
+
+drop table if exists `ebook_snapshot`;
+create table `ebook_snapshot` (
+    `id` bigint auto_increment not null comment 'id',
+    `ebook_id` bigint not null default 0 comment 'ebookId',
+    `date` date not null comment '快照日期',
+    `view_count` int not null default 0 comment '阅读数',
+    `vote_count` int not null default 0 comment '点赞数',
+    `view_increase` int not null default 0 comment '阅读增长',
+    `vote_increase` int not null default 0 comment '点赞增长',
+    primary key (`id`),
+    unique key `ebook_id_date_unique` (`ebook_id`)
+)  engine = innodb default charset = utf8mb4 comment 'ebook快照';
